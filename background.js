61d9e3c26db1148692bfc16bff6284370fcee653
Surendar Yama <surendaryama@outlook.com>
2025-04-29 13:18:44 +0530

    Open with Youtube Google Chrome Extension: shortcut added.

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
-        id: "000000000017",
-        title: 'Open with youtube',
-        contexts: ['selection'],
+        id: "open_with_youtube",
+        title: "Open with youtube (Ctrl+Y)",
+        contexts: ["selection"],
     });
 });
 const handleOnClick = ({selectionText}, _) => {
@@ -10,3 +10,26 @@ const handleOnClick = ({selectionText}, _) => {
     chrome.tabs.create({ url });
 };
 chrome.contextMenus.onClicked.addListener(handleOnClick);
+
+chrome.commands.onCommand.addListener((command) => {
+    if (command === 'open_with_youtube') {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTabId = tabs[0].id;
+            chrome.scripting.executeScript(
+                {
+                    target: { tabId: activeTabId },
+                    func: () => window.getSelection().toString(),
+                },
+                (results) => {
+                    const selectionText = results[0].result;
+                    if (selectionText) {
+                        const url = `https://www.youtube.com/results?search_query=${selectionText}`;
+                        chrome.tabs.create({ url });
+                    } else {
+                        console.log('No text selected.');
+                    }
+                }
+            );
+        });
+    }
+});
