641fd08fda24b897ec22f53db26d15c8d3754ef2
Surendar Yama <surendaryama@outlook.com>
2025-05-11 12:16:44 +0530

    querySearch Fn added 😍.

--- background.js
+++ background.js
@@ -1,35 +1,36 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: "open_with_youtube",
-        title: "Open with youtube (Ctrl+Y)",
-        contexts: ["selection"],
-    });
+  chrome.contextMenus.create({
+    id: "open_with_youtube",
+    title: "Open with youtube (Ctrl+Y)",
+    contexts: ["selection"],
+  });
 });
-const handleOnClick = ({selectionText}, _) => {
-    const url = `https://www.youtube.com/results?search_query=${selectionText}`;
-    chrome.tabs.create({ url });
+
+const querySearch = (selectionText) => {
+  const url = `https://www.youtube.com/results?search_query=${selectionText}`;
+  chrome.tabs.create({ url });
 };
+const handleOnClick = ({ selectionText }, _) => querySearch(selectionText);
 chrome.contextMenus.onClicked.addListener(handleOnClick);
 
 chrome.commands.onCommand.addListener((command) => {
-    if (command === 'open_with_youtube') {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            const activeTabId = tabs[0].id;
-            chrome.scripting.executeScript(
-                {
-                    target: { tabId: activeTabId },
-                    func: () => window.getSelection().toString(),
-                },
-                (results) => {
-                    const selectionText = results[0].result;
-                    if (selectionText) {
-                        const url = `https://www.youtube.com/results?search_query=${selectionText}`;
-                        chrome.tabs.create({ url });
-                    } else {
-                        console.log('No text selected.');
-                    }
-                }
-            );
-        });
-    }
+  if (command === "open_with_youtube") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTabId = tabs[0].id;
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: activeTabId },
+          func: () => window.getSelection().toString(),
+        },
+        (results) => {
+          const selectionText = results[0].result;
+          if (selectionText) {
+            querySearch(selectionText);
+          } else {
+            console.log("No text selected.");
+          }
+        }
+      );
+    });
+  }
 });
